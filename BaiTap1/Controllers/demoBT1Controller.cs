50d10a5bb1c3b1f6fedef31c6b1c36f1349e5022
ADMIN <ADMIN@DESKTOP-HGBFNF7>
2021-09-03 14:55:05 +0700

    Add project files.

---
+++ BaiTap1/Controllers/demoBT1Controller.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BaiTap1.Controllers
+{
+    public class demoBT1Controller : Controller
+    {
+        // GET: demoBT1
+        public ActionResult GPT()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult GPT(string soA, string soB)
+        {
+            double soa = Convert.ToDouble(soA);
+            double sob = Convert.ToDouble(soB);
+            double ketqua = -sob / soa;
+            ViewBag.Giaipt = ketqua;
+            return View();
+        }
+    }
+}
